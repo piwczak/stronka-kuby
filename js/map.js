a72c79e31b824db52ec239e9e8522ff5beeed21d
PrzemekPiwowarczyk <przemek.piwowarczyk@gmail.com>
2017-03-13 22:44:09 +0100

    Stronka Kuby ver. 1.0

---
+++ js/map.js
@@ -0,0 +1,53 @@
+﻿'use strict';
+
+var MapModule = (function($) {
+    var initMap = function(google) {
+        var latLang = { lat: 50.175347, lng: 20.236888 }
+        var mapSettings = {
+            center: latLang,
+            zoom: 10
+        };
+        var map = new google.maps.Map($('#map')[0], mapSettings);
+
+        var markerSettings = {
+            position: latLang,
+            map: map,
+            title: 'Pizzeria u Jakuba'
+        }
+
+        var infoWindow = new google.maps.InfoWindow({
+            content: 'Pizzeria u Jakuba</br>Adres</br>'
+        });
+
+        var marker = new google.maps.Marker(markerSettings);
+        marker.addListener('click',
+            function () {
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+                setTimeout(function() {
+                        marker.setAnimation(null);
+                        infoWindow.open(map, marker);
+                    },
+                    700);
+
+            });
+
+        var circleBorder = new google.maps.Circle({
+            strokeColor: '#ff0000',
+            strokeOpacity: 0.2,
+            strokeWeight: 2,
+            fillColor: '#ff0000',
+            fillOpacity: 0.2,
+            map: map,
+            center: latLang,
+            radius: 5000
+        });
+    }
+
+    return {
+        InitMap: initMap
+    }
+})(jQuery);
+
+function initMap() {
+    return MapModule.InitMap(google);
+}
---
+++ js/script.js
@@ -0,0 +1,59 @@
+﻿'use strict';
+
+$(function () {
+    let windowWidth = $(window).width();
+    var offset = 0;
+
+    if(windowWidth <= 360) {
+        offset = 175;
+    }
+    else if (windowWidth > 360 && windowWidth <= 768) {
+        offset = 170;
+    } else {
+        offset = 135;
+    }
+
+    $('body')
+        .scrollspy({
+            target: '.navbar',
+            offset: offset
+        });
+
+    $('nav a, .down-button a')
+        .on('click',
+            function (event) {
+                $('body, html')
+                    .stop()
+                    .animate({
+                        scrollTop: $($(this).attr('href')).offset().top - 100
+                    }, 1500, 'easeInOutExpo');
+                event.preventDefault();
+            });
+
+    $('#theCarousel').carousel();
+
+    var animatedNavbar = (function($) {
+        var headerOffset = 300;
+        var elem = $('.scroll-navbar');
+
+        var init = function() {
+            $(window).on('scroll',
+                function() {
+                    setTimeout(navbarOnScrolling, 250);
+                });
+        }
+
+
+        var navbarOnScrolling = function() {
+            var scrollY = window.pageYOffset || document.scrollTop;
+
+            if (scrollY >= headerOffset) {
+                elem.addClass('shrink-navbar');
+            } else {
+                elem.removeClass('shrink-navbar');
+            }
+        }
+
+        init();
+    })($);
+});
